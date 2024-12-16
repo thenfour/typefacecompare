6b671e760602c6746b435a94735fd7e5a8be4b50
tenfour <carlco@gmail.com>
2024-12-16 23:55:07 +0100

    - can hide font names - formatting tweaks

--- src/components/DefaultConfig.tsx
+++ src/components/DefaultConfig.tsx
@@ -1,26 +1,64 @@
-export const gDefaultTypefaceCompareConfig ={
+// !-- MC Rilego by Maulana Creative
+// !-- Mundo Serif™ by Monotype
+// !-- Egyptian Slate™ by Monotype
+// !-- PMN Caecilia® by Monotype
+// #-- Noemí Slab by Brackets
+// xxx TheSerif by LucasFonts
+
+// x-- Bandera Pro™ by AndrijType free
+// x-- Justus Pro™ by URW Type Foundry
+// x-- Adagio Slab by Machalski
+// x-- Shandon Slab by Hoftype
+// x-- Foro Rounded by Hoftype
+// x-- Emy Slab by Latinotype
+// x-- Multiple by Latinotype
+// x-- Palo Slab by TypeUnion
+
+
+
+// {
+//     "sizeScale": 1.23,
+//     "weightScale": 1,
+//     "fontFamily": "MaestroTimes",
+//     "notes": "",
+//     "tags": "",
+//     "enabled": true,
+//     "sortOrder": 34
+//  },
+//  {
+//     "sizeScale": 1.12,
+//     "weightScale": 1,
+//     "fontFamily": "Jutlandia Slab",
+//     "enabled": true,
+//     "notes": "",
+//     "tags": "",
+//     "sortOrder": 35
+//  }
+
+
+export const gDefaultTypefaceCompareConfig = {
     "specimens": [
        {
-          "sizeScale": 2.1,
-          "text": "Paper Spaceships",
-          "weightScale": 1,
+          "sizeScale": 2.5,
+          "text": "El Cumbanchero",
+          "weightScale": 2.15,
           "lineHeight": 20,
           "lineHeightEnable": false,
-          "marginBottom": 9,
-          "marginTop": 0,
-          "enabled": false
+          "marginBottom": 62,
+          "marginTop": 70,
+          "enabled": true
        },
        {
           "sizeScale": 1,
-          "text": "Play 3 times. To coda with Mute; tutti, hi-hat",
+          "text": "Play 3 times. To coda with Mute; tutti, hi-hat. Lg",
           "weightScale": 1,
           "marginBottom": 0,
           "marginTop": 0,
-          "enabled": false
+          "enabled": true
        },
        {
-          "sizeScale": 3.1,
-          "text": "0123456789 CFBEADG grambithq AHWMIK",
+          "sizeScale": 1.7,
+          "text": "F7 C11 grahmbitq AHWMIK C1F2B3E4A5D6G7890",
           "weightScale": 1,
           "marginBottom": 0,
           "marginTop": 16,
@@ -28,32 +66,48 @@ export const gDefaultTypefaceCompareConfig ={
        },
        {
           "sizeScale": 1,
-          "text": "G7 . . Dsus4 . . F7alt . . Em9 . . A13 . . B5 . . C11 . . E2",
+          "text": "F7 G6 Dsus4 . . C7alt . . Em9 . . A13 . . B5 . . C11 . . E2",
           "weightScale": 1,
           "marginBottom": 0,
           "marginTop": 0,
-          "enabled": true
+          "enabled": false
+       },
+       {
+          "sizeScale": 1,
+          "text": "She has a great job, she worked her way up and is doing well for herself",
+          "weightScale": 1,
+          "marginBottom": 0,
+          "marginTop": 0,
+          "enabled": false
+       },
+       {
+          "sizeScale": 1,
+          "text": "F7 C11 Maghom Kletx C1F2B3E4A5D6G7890",
+          "weightScale": 1,
+          "marginBottom": 0,
+          "marginTop": 0,
+          "enabled": false
        }
     ],
-    "fontSize": 38,
+    "fontSize": 15,
     "fontWeight": 400,
     "fonts": [
        {
-          "sizeScale": 1.07,
+          "sizeScale": 1.06,
           "weightScale": 1,
-          "fontFamily": "Literata",
+          "fontFamily": "Noto Serif",
+          "enabled": true,
           "notes": "",
           "tags": "",
-          "enabled": true,
           "sortOrder": 0
        },
        {
-          "sizeScale": 1.24,
+          "sizeScale": 1.19,
           "weightScale": 1,
-          "fontFamily": "Times New Roman",
+          "fontFamily": "Cambria",
+          "enabled": true,
           "notes": "",
           "tags": "",
-          "enabled": true,
           "sortOrder": 1
        },
        {
@@ -66,211 +120,31 @@ export const gDefaultTypefaceCompareConfig ={
           "sortOrder": 2
        },
        {
-          "sizeScale": 1.16,
-          "weightScale": 1,
-          "fontFamily": "TT Rationalist Trl",
-          "notes": "",
-          "tags": "",
-          "enabled": true,
-          "sortOrder": 3
-       },
-       {
-          "sizeScale": 1.14,
-          "weightScale": 1,
-          "fontFamily": "Adelle Sans",
-          "enabled": true,
-          "notes": "chords Gtail",
-          "tags": "",
-          "sortOrder": 4
-       },
-       {
-          "sizeScale": 1.13,
-          "weightScale": 1,
-          "fontFamily": "Trade Gothic LT Std",
-          "enabled": true,
-          "notes": "",
-          "tags": "",
-          "sortOrder": 5
-       },
-       {
-          "sizeScale": 1.22,
-          "weightScale": 1,
-          "fontFamily": "Source Sans Pro",
-          "enabled": true,
-          "notes": "",
-          "tags": "",
-          "sortOrder": 6
-       },
-       {
           "sizeScale": 1.12,
           "weightScale": 1,
-          "fontFamily": "Fira Sans",
+          "fontFamily": "Heuristica",
           "enabled": true,
           "notes": "",
           "tags": "",
-          "sortOrder": 7
-       },
-       {
-          "sizeScale": 1.14,
-          "weightScale": 1,
-          "fontFamily": "Segoe UI Variable Text",
-          "enabled": true,
-          "notes": "",
-          "tags": "",
-          "sortOrder": 8
-       },
-       {
-          "sizeScale": 1.15,
-          "weightScale": 1,
-          "fontFamily": "LATO",
-          "enabled": true,
-          "notes": "",
-          "tags": "",
-          "sortOrder": 9
-       },
-       {
-          "sizeScale": 1.08,
-          "weightScale": 1,
-          "fontFamily": "Noto Sans",
-          "enabled": true,
-          "notes": "",
-          "tags": "",
-          "sortOrder": 10
-       },
-       {
-          "sizeScale": 1.14,
-          "weightScale": 1,
-          "fontFamily": "Roboto",
-          "enabled": true,
-          "notes": "",
-          "tags": "",
-          "sortOrder": 11
-       },
-       {
-          "sizeScale": 1.13,
-          "weightScale": 1,
-          "fontFamily": "Avenir Next LT Pro Regular",
-          "enabled": true,
-          "notes": "",
-          "tags": "",
-          "sortOrder": 12
-       },
-       {
-          "sizeScale": 1.09,
-          "weightScale": 1,
-          "fontFamily": "Azo Sans Test",
-          "enabled": true,
-          "notes": "",
-          "tags": "",
-          "sortOrder": 13
-       },
-       {
-          "sizeScale": 1.21,
-          "weightScale": 1,
-          "fontFamily": "Jost",
-          "enabled": true,
-          "notes": "",
-          "tags": "",
-          "sortOrder": 14
-       },
-       {
-          "sizeScale": 1.16,
-          "weightScale": 1,
-          "fontFamily": "Arial",
-          "enabled": true,
-          "notes": "",
-          "tags": "",
-          "sortOrder": 15
-       },
-       {
-          "sizeScale": 1.16,
-          "weightScale": 1,
-          "fontFamily": "Acumin Pro",
-          "notes": "",
-          "tags": "",
-          "enabled": true,
-          "sortOrder": 16
+          "sortOrder": 3
        },
        {
           "sizeScale": 1.07,
           "weightScale": 1,
-          "fontFamily": "Inter",
+          "fontFamily": "Literata",
           "notes": "",
           "tags": "",
           "enabled": true,
-          "sortOrder": 17
+          "sortOrder": 4
        },
        {
-          "sizeScale": 1,
+          "sizeScale": 1.24,
           "weightScale": 1,
-          "fontFamily": "DejaVu Sans",
-          "enabled": true,
+          "fontFamily": "Times New Roman",
           "notes": "",
           "tags": "",
-          "sortOrder": 18
-       },
-       {
-          "sizeScale": 1.19,
-          "weightScale": 1,
-          "fontFamily": "PT Sans",
           "enabled": true,
-          "notes": "",
-          "tags": "",
-          "sortOrder": 19
-       },
-       {
-          "sizeScale": 1,
-          "weightScale": 1,
-          "fontFamily": "Verdana",
-          "enabled": true,
-          "notes": "",
-          "tags": "",
-          "sortOrder": 20
-       },
-       {
-          "sizeScale": 1.23,
-          "weightScale": 1,
-          "fontFamily": "Dubai",
-          "enabled": true,
-          "notes": "",
-          "tags": "",
-          "sortOrder": 21
-       },
-       {
-          "sizeScale": 1.14,
-          "weightScale": 1,
-          "fontFamily": "Overpass",
-          "enabled": true,
-          "notes": "",
-          "tags": "",
-          "sortOrder": 22
-       },
-       {
-          "sizeScale": 1.15,
-          "weightScale": 1,
-          "fontFamily": "Optima",
-          "enabled": true,
-          "notes": "",
-          "tags": "",
-          "sortOrder": 23
-       },
-       {
-          "sizeScale": 1.12,
-          "weightScale": 1,
-          "fontFamily": "Quicksand",
-          "enabled": true,
-          "notes": "",
-          "tags": "",
-          "sortOrder": 24
-       },
-       {
-          "sizeScale": 1.12,
-          "weightScale": 1,
-          "fontFamily": "Jutlandia Slab",
-          "enabled": true,
-          "notes": "",
-          "tags": "",
-          "sortOrder": 25
+          "sortOrder": 5
        },
        {
           "sizeScale": 1.1,
@@ -279,7 +153,97 @@ export const gDefaultTypefaceCompareConfig ={
           "notes": "",
           "tags": "",
           "enabled": true,
-          "sortOrder": 26
+          "sortOrder": 6
+       },
+       {
+          "sizeScale": 1.07,
+          "weightScale": 1,
+          "fontFamily": "Roboto Slab",
+          "enabled": true,
+          "notes": "",
+          "tags": "",
+          "sortOrder": 7
+       },
+       {
+          "sizeScale": 1.18,
+          "weightScale": 1,
+          "fontFamily": "Amasis MT W1G",
+          "enabled": true,
+          "notes": "",
+          "tags": "",
+          "sortOrder": 8
+       },
+       {
+          "sizeScale": 1.09,
+          "weightScale": 0.89,
+          "fontFamily": "Bandera Pro",
+          "enabled": true,
+          "notes": "",
+          "tags": "",
+          "sortOrder": 9
+       },
+       {
+          "sizeScale": 1.08,
+          "weightScale": 1,
+          "fontFamily": "Justus Pro",
+          "enabled": true,
+          "notes": "",
+          "tags": "",
+          "sortOrder": 10
+       },
+       {
+          "sizeScale": 1.16,
+          "weightScale": 1,
+          "fontFamily": "TT Rationalist Trl",
+          "notes": "",
+          "tags": "",
+          "enabled": true,
+          "sortOrder": 11
+       },
+       {
+          "sizeScale": 1.13,
+          "weightScale": 0.84,
+          "fontFamily": "Rockwell",
+          "enabled": true,
+          "notes": "",
+          "tags": "",
+          "sortOrder": 12
+       },
+       {
+          "sizeScale": 1.06,
+          "weightScale": 1,
+          "fontFamily": "Petaluma Text",
+          "enabled": true,
+          "notes": "",
+          "tags": "",
+          "sortOrder": 13
+       },
+       {
+          "sizeScale": 1.38,
+          "weightScale": 1,
+          "fontFamily": "Perpetua",
+          "enabled": true,
+          "notes": "",
+          "tags": "",
+          "sortOrder": 14
+       },
+       {
+          "sizeScale": 1.12,
+          "weightScale": 1,
+          "fontFamily": "Georgia",
+          "enabled": true,
+          "notes": "",
+          "tags": "",
+          "sortOrder": 15
+       },
+       {
+          "sizeScale": 1.29,
+          "weightScale": 1,
+          "fontFamily": "Garamond",
+          "enabled": true,
+          "notes": "",
+          "tags": "",
+          "sortOrder": 16
        },
        {
           "sizeScale": 1.26,
@@ -288,7 +252,7 @@ export const gDefaultTypefaceCompareConfig ={
           "notes": "",
           "tags": "",
           "enabled": true,
-          "sortOrder": 27
+          "sortOrder": 17
        },
        {
           "sizeScale": 1.13,
@@ -297,24 +261,205 @@ export const gDefaultTypefaceCompareConfig ={
           "notes": "",
           "tags": "",
           "enabled": true,
+          "sortOrder": 18
+       },
+       {
+          "sizeScale": 1.15,
+          "weightScale": 1,
+          "fontFamily": "LATO",
+          "enabled": true,
+          "notes": "",
+          "tags": "",
+          "sortOrder": 19
+       },
+       {
+          "sizeScale": 1.13,
+          "weightScale": 1,
+          "fontFamily": "Trade Gothic LT Std",
+          "enabled": true,
+          "notes": "",
+          "tags": "",
+          "sortOrder": 20
+       },
+       {
+          "sizeScale": 1.22,
+          "weightScale": 1,
+          "fontFamily": "Source Sans Pro",
+          "enabled": true,
+          "notes": "",
+          "tags": "",
+          "sortOrder": 21
+       },
+       {
+          "sizeScale": 1.08,
+          "weightScale": 1,
+          "fontFamily": "Noto Sans",
+          "enabled": true,
+          "notes": "",
+          "tags": "",
+          "sortOrder": 22
+       },
+       {
+          "sizeScale": 1.09,
+          "weightScale": 1,
+          "fontFamily": "Azo Sans Test",
+          "enabled": true,
+          "notes": "",
+          "tags": "",
+          "sortOrder": 23
+       },
+       {
+          "sizeScale": 1.16,
+          "weightScale": 1,
+          "fontFamily": "Arial",
+          "enabled": true,
+          "notes": "",
+          "tags": "",
+          "sortOrder": 24
+       },
+       {
+          "sizeScale": 1.16,
+          "weightScale": 1,
+          "fontFamily": "Acumin Pro",
+          "notes": "",
+          "tags": "",
+          "enabled": true,
+          "sortOrder": 25
+       },
+       {
+          "sizeScale": 1.14,
+          "weightScale": 1,
+          "fontFamily": "Roboto",
+          "enabled": true,
+          "notes": "",
+          "tags": "",
+          "sortOrder": 26
+       },
+       {
+          "sizeScale": 1.12,
+          "weightScale": 1,
+          "fontFamily": "Fira Sans",
+          "enabled": true,
+          "notes": "",
+          "tags": "",
+          "sortOrder": 27
+       },
+       {
+          "sizeScale": 1.07,
+          "weightScale": 1,
+          "fontFamily": "Inter",
+          "notes": "",
+          "tags": "",
+          "enabled": true,
           "sortOrder": 28
+       },
+       {
+          "sizeScale": 1.14,
+          "weightScale": 1,
+          "fontFamily": "Adelle Sans",
+          "enabled": true,
+          "notes": "chords Gtail",
+          "tags": "",
+          "sortOrder": 29
+       },
+       {
+          "sizeScale": 1.14,
+          "weightScale": 1,
+          "fontFamily": "Segoe UI Variable Text",
+          "enabled": true,
+          "notes": "",
+          "tags": "",
+          "sortOrder": 30
+       },
+       {
+          "sizeScale": 1.19,
+          "weightScale": 1,
+          "fontFamily": "PT Sans",
+          "enabled": true,
+          "notes": "",
+          "tags": "",
+          "sortOrder": 31
+       },
+       {
+          "sizeScale": 1.21,
+          "weightScale": 1,
+          "fontFamily": "Jost",
+          "enabled": true,
+          "notes": "",
+          "tags": "",
+          "sortOrder": 32
+       },
+       {
+          "sizeScale": 1,
+          "weightScale": 1,
+          "fontFamily": "DejaVu Sans",
+          "enabled": true,
+          "notes": "",
+          "tags": "",
+          "sortOrder": 33
+       },
+       {
+          "sizeScale": 1.13,
+          "weightScale": 1,
+          "fontFamily": "Avenir Next LT Pro Regular",
+          "enabled": true,
+          "notes": "",
+          "tags": "",
+          "sortOrder": 34
+       },
+       {
+          "sizeScale": 1,
+          "weightScale": 1,
+          "fontFamily": "Verdana",
+          "enabled": true,
+          "notes": "",
+          "tags": "",
+          "sortOrder": 35
        },
        {
           "sizeScale": 1.23,
           "weightScale": 1,
-          "fontFamily": "MaestroTimes",
+          "fontFamily": "Dubai",
+          "enabled": true,
           "notes": "",
           "tags": "",
+          "sortOrder": 36
+       },
+       {
+          "sizeScale": 1.14,
+          "weightScale": 1,
+          "fontFamily": "Overpass",
           "enabled": true,
-          "sortOrder": 29
+          "notes": "",
+          "tags": "",
+          "sortOrder": 37
+       },
+       {
+          "sizeScale": 1.15,
+          "weightScale": 1,
+          "fontFamily": "Optima",
+          "enabled": true,
+          "notes": "",
+          "tags": "",
+          "sortOrder": 38
+       },
+       {
+          "sizeScale": 1.12,
+          "weightScale": 1,
+          "fontFamily": "Quicksand",
+          "enabled": true,
+          "notes": "",
+          "tags": "",
+          "sortOrder": 39
        }
     ],
-    "showCards": false,
+    "showCards": true,
     "showOneLiners": true,
     "showNotation": false,
-    "showRehearsalMarks": false,
+    "showRehearsalMarks": true,
     "showChords": true,
     "globalNotes": "## Chords\n\n* \"C\" should be closed (Segoe too open)\n* \"D\" must be fully rounded\n* \"11\" must have balanced rhythm (Fira \"C11\" too cramped)\n* \"F7\" should not be a ligature. This is one reason serif fonts may be preferred (Caslon)\n* Rounds should not be circular. For space reasons, and because it's dissonance. See Avenir's G and C are too bulky because they try to be circular.\n* Balanced stroke widths. Serif: Caslon \n* Numbers and letters to flow\n",
     "showFontConfig": false,
-    "showSpecimenConfig": true
+    "showSpecimenConfig": false,
+    "showFontNames": true
  }
--- src/components/MarkdownWrapper.tsx
+++ src/components/MarkdownWrapper.tsx
@@ -2,29 +2,29 @@ import React, { useEffect, useState } from 'react';
 import MarkdownIt from 'markdown-it';
 
 interface MarkdownWrapperProps {
-  /**
-   * The raw Markdown string to render.
-   */
-  markdown: string;
-  /**
-   * Optional MarkdownIt configuration options.
-   * https://markdown-it.github.io/markdown-it/
-   */
-  //options?: MarkdownIt.Options;
+    /**
+     * The raw Markdown string to render.
+     */
+    markdown: string;
+    /**
+     * Optional MarkdownIt configuration options.
+     * https://markdown-it.github.io/markdown-it/
+     */
+    //options?: MarkdownIt.Options;
 }
 
 export const Markdown: React.FC<MarkdownWrapperProps> = ({ markdown }) => {
-  const [html, setHtml] = useState<string>('');
+    const [html, setHtml] = useState<string>('');
 
-  useEffect(() => {
-    const md = new MarkdownIt();
-    const renderedHtml = md.render(markdown);
-    setHtml(renderedHtml);
-  }, [markdown]);
+    useEffect(() => {
+        const md = new MarkdownIt();
+        const renderedHtml = md.render(markdown);
+        setHtml(renderedHtml);
+    }, [markdown]);
 
-  return (
-    <div className='renderedMarkdown' dangerouslySetInnerHTML={{ __html: html }} />
-  );
+    return (
+        <div className='renderedMarkdown' dangerouslySetInnerHTML={{ __html: html }} />
+    );
 };
 
 
@@ -38,8 +38,10 @@ interface MarkdownEditorProps {
 export const MarkdownEditor = (props: MarkdownEditorProps) => {
     const [value, setValue] = useState<string>(props.initialValue);
     return <div className='markdownEditor'>
-        <textarea className="globalNotesTextArea" value={value} onChange={e => setValue(e.target.value)}></textarea>
-        <Markdown markdown={value} />
+        <div className='twoColumn'>
+            <textarea className="globalNotesTextArea" value={value} onChange={e => setValue(e.target.value)}></textarea>
+            <Markdown markdown={value} />
+        </div>
         <button onClick={e => props.onSave(value)}>Save</button>
         <button onClick={props.onCancel}>Cancel</button>
     </div>;
